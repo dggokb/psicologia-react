2e664e5796db7d5eb5bc19d7980af7ed7abea2c5
DiegoGarcez <dggokb@gmail.com>
2023-09-26 17:41:36 -0400

    adicionando teste de post

---
+++ src/pacienteTestePost.jsx
@@ -0,0 +1,68 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
+
+export default function PacienteTestePost() { 
+
+  const [nome, setNome] = useState();
+  const [endereco, setEndereco] = useState();
+  const [quantidaDeDiasNoMes, setQuantidaDeDiasNoMes] = useState();
+  const [valorPorSessao, setValorPorSessao] = useState();
+  const [mes, setMes] = useState();
+  const [ano, setAno] = useState();
+  const [tipo, setTipo] = useState();
+
+  const mutation = useMutation(
+    (novoPaciente) => {
+      return axios.post("http://localhost:8080/paciente", novoPaciente);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('paciente-data');
+      },
+    }
+  );
+
+  const criarPaciente = (e) => {
+    e.preventDefault();
+    const novoPaciente = {
+      nome,
+      endereco,
+      quantidaDeDiasNoMes,
+      valorPorSessao,
+      mes,
+      ano,
+      tipo
+    };
+    mutation.mutate(novoPaciente);
+  };
+  
+  return (  
+    <div>    
+      <form onSubmit={criarPaciente}>
+        <div>
+          <span>Nome:</span><input type="text" onChange={(e) => setNome(e.target.value)} />
+        </div>        
+        <div>
+          <span>Endereço:</span><input type="text" onChange={(e) => setEndereco(e.target.value)} />
+        </div>   
+        <div>
+          <span>Quantidade de dias no mês:</span><input type="number" onChange={(e) => setQuantidaDeDiasNoMes(e.target.value)} />
+        </div>  
+        <div>
+          <span>Valor por sessão:</span><input type="number" onChange={(e) => setValorPorSessao(e.target.value)} />
+        </div>  
+        <div>
+          <span>Mês:</span><input type="text" onChange={(e) => setMes(e.target.value)} />
+        </div>  
+        <div>
+          <span>Ano:</span><input type="number" onChange={(e) => setAno(e.target.value)} />
+        </div>  
+        <div>
+          <span>Tipo:</span><input type="text" onChange={(e) => setTipo(e.target.value)} />
+        </div>  
+        <input type="submit" value={mutation.isSuccess ? "Salvo!" : "Salvar"} />
+      </form>
+    </div>
+  )
+}
