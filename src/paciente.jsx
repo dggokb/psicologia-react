fb0230ab88877d57ab920450c9aa88c0c60e0020
DiegoGarcez <dggokb@gmail.com>
2023-09-26 17:40:46 -0400

    mapeando vetor dentro do vetor de valores do paciente

--- src/paciente.jsx
+++ src/paciente.jsx
@@ -16,7 +16,16 @@ export default function Paciente() {
                 <p>Nome: {dado.nome}</p>
                 <p>Endereço: {dado.endereco}</p>
                 <p>Data de inicio: {dado.dataDeInicio}</p>
-                
+                <span>-----Valores:-----</span>
+                {dado.valores.map((valor) =>(
+                  <div>
+                    <p>Quantidade de dias no mês: {valor.quantidaDeDiasNoMes}</p>
+                    <p>Valor por sessão: {valor.valorPorSessao}</p>
+                    <p>Mês: {valor.mes}</p>
+                    <p>Ano: {valor.ano}</p>
+                  </div>                  
+                ))}
+                <span>-----Valores:-----</span>
                 <p>Tipo: {dado.tipo}</p>
                 <span>---------------------</span>
               </div>
