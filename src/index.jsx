2a7b65ad37365e12bdfc9c94f215ad87e158f230
DiegoGarcez <dggokb@gmail.com>
2023-09-29 11:16:13 -0400

    criando rota inicial no index

--- src/index.jsx
+++ src/index.jsx
@@ -4,23 +4,36 @@ import {
 } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PacienteTestePost from './components/pacientePost';
-import PacienteTesteSelect from './components/selectDeTipoDePaciente';
-import PacienteGet from './components/pacienteGet';
-
-
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './error-page';
+import Root, { action as rootAction, loader as rootLoader } from './routes/root';
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader(queryClient),
+    action: rootAction(queryClient),
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+    ],
+  },
+])
+
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient} contextSharing={true}>
-      <PacienteTestePost>
-        <PacienteTesteSelect />
-      </PacienteTestePost>
-      <PacienteGet />
-    </QueryClientProvider>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient} contextSharing={true}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools position="bottom-right" />
+      </QueryClientProvider>
+    </React.StrictMode>
   )
 }
 
