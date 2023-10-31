42c11788cefc42078aa72d7d791a12a5918ee972
DiegoGarcez <dggokb@gmail.com>
2023-10-31 16:13:42 -0400

    adicionando rota com drop no navbar

--- src/routes/navbar.tsx
+++ src/routes/navbar.tsx
@@ -16,7 +16,7 @@ export default function BarraDeNavegacao() {
                         <div className="p-2">
                             <NavDropdown title="Adicionar" id="basic-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="adicionar">Novo Paciente</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="sessoes">Novas sessões</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="sessoes">Novas sessões do paciente</NavDropdown.Item>
                             </NavDropdown>
                         </div>
                         <div className="p-2">
